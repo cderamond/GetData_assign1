3bfdeeb49020f7806d29fc7beb61b5f81eb38a19
cderamond <cderamond@gmail.com>
2014-09-14 23:37:54 -0300

    major upload, of code, and data.
    Signed-off-by: cderamond <cderamond@gmail.com>

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ note that by coursera honor code, you must make sure the code you submit is your
 xxx.R does
 xxxy.R does
 
+License:
+========
+Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+
---
+++ run_analysis.R
@@ -0,0 +1,34 @@
+cat("loading required packages")
+library(dplyr)
+library(tidyr)
+library(data.table) #loading required packages
+
+
+rData <- function(x, y, sub) {
+        # this function will take the data set, assign the names and the right columns
+        if(!file.exists("./UCI HAR Dataset")){stop("data set directory does not exist, please check instructions") }
+        x <- <- read.table(x, quote="\"", stringsAsFactors=FALSE)
+        features <- read.table("./UCI HAR Dataset/features.txt", quote="\"", stringsAsFactors=FALSE) #read column names
+        names(x) <- features$V2
+        #next lines subsets
+                x <- select(x, contains("-mean()"), contains("-std()"))
+                y <- read.table(y, quote="\"", stringsAsFactors=FALSE)
+                names(y) <- "activity_id"
+                x <- cbind(y, x) # now we have the right set with means and std of x plus the activity
+        # now we attach the subject ID
+                        sub <- read.table(sub, quote="\"", stringsAsFactors=FALSE)
+                        names(sub) <- "subject_ID"
+                        x <- cbind(sub, x)
+        
+}
+yTest <- "./UCI HAR Dataset/test/y_test.txt"
+xTest <- "./UCI HAR Dataset/test/X_test.txt"
+subTest <- "./UCI HAR Dataset/test/subject_test.txt"
+yTrain <- "./UCI HAR Dataset/train/y_train.txt"
+xTrain <- "./UCI HAR Dataset/train/X_train.txt"
+
+
+
+
+#View(set_test)
+
